80bffaf6c5f39da7c33538c551281d8684c9eac9
Sam <samanthaasampot@gmail.com>
2024-05-27 14:04:24 +0800

    Added logic for comparing extracted colors with the 12 main colors

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
 import cv2
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt 
+import math
 
-def extract_palette(image_path, num_colors=10):
+def extract_palette(image_path, num_colors=5):
      
      # Load image and convert to RGB
      image = cv2.imread(image_path)
@@ -21,22 +23,65 @@ def extract_palette(image_path, num_colors=10):
 
 def display_palette(colors):
      # Create a figure and axes
-     fig, ax = plt.subplots(figsize=(10,2), subplot_kw=dict(xticks=[], yticks=[], frame_on=False))
+     fig, ax = plt.subplots(figsize=(5,2), subplot_kw=dict(xticks=[], yticks=[], frame_on=False))
+     hex_strings = []
      
      # Display the palette
      for i, color in enumerate(colors):
-          hex_string = '#%02x%02x%02x' % tuple(color.reshape(1, -1)[0])
+          hex_string = '#%02x%02x%02x' % tuple(color)
           ax.add_patch(plt.Rectangle((i, 0), 1, 1, color=hex_string))
+          hex_strings.append(hex_string)
           
      # Adjust the plot limits and show the palette
      ax.set_xlim(0, len(colors))
      ax.set_ylim(0, 1)
      plt.show()
+     
+     return hex_strings
 
+def get_closest_color(hex_strings):
+     
+     colors = [
+          '#fc0000', # Red
+          '#ff7b00', # Orange
+          '#ffff00', # Yellow
+          '#7bff00', # Yellow Green
+          '#00ff00', # Green
+          '#00ff7d', # Turquoise
+          '#00ffff', # Cyan
+          '#007bff', # Ocean
+          '#0000ff', # Blue
+          '#7b00ff', # Violet
+          '#ff00ff', # Magenta
+          '#ff007b' # Raspberry
+     ]
+     
+     closest_colors = []
+
+     for hex_string in hex_strings:
+          input_color = tuple(int(hex_string.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+          min_distance = float('inf')
+          closest_hex = None
+
+          for color in colors:
+               base_color = tuple(int(color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+               distance = math.sqrt(sum([(a - b) ** 2 for a, b in zip(base_color, input_color)]))
+               
+               if distance < min_distance:
+                    min_distance = distance
+                    closest_hex = color
+
+          closest_colors.append(closest_hex)
+          print(f"Closest color to {hex_string} is {closest_hex}")
+
+     return closest_colors
+          
 def main():
      
-     colors = extract_palette('sample2.jpg')
-     display_palette(colors)
+     colors = extract_palette('samples/sample3.jpg')
+     hex = display_palette(colors)
+     
+     get_closest_color(hex)
           
 if __name__ == "__main__":
      main()
